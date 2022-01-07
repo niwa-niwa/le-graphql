cc378c2af026936edbf9caf681fd88c336afce36
niwa-niwa <urban.hang274@gmail.com>
2022-01-08 00:07:36 +0900

    implemented index.ts

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  # gql can be wrote comments in gql syntax
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
